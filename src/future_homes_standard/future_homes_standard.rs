fcb7e619edc2ff128dce99efb0f93c720733fbfc
rachowell <rachel.howell@madetech.com>
2026-03-13 17:15:07 +0000

    EC-1172 add comment to create_appliance_gains
    Re difference in sorting to the Python.

--- src/future_homes_standard/future_homes_standard.rs
+++ src/future_homes_standard/future_homes_standard.rs
@@ -2468,6 +2468,8 @@ fn create_appliance_gains(
 
     // Assign priority to those with a kWhcycle value, in reverse order
     for (priority, appliance) in appliance_kwhcycle
+        // the Python behaviour differs here as it sorts by index 1 (2nd letter) of the appliance key string,
+        // in the Rust we've implemented what we think is the intended behaviour and reported the bug to DESNZ
         .sorted_by(|_, v1, _, v2| v1.total_cmp(v2))
         .rev()
         .enumerate()
