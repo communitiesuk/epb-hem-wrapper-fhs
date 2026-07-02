2e77513bc9d598806ccabd11712ad323fd3c601a
Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>
2026-07-02 15:55:15 +0100

    Begin refactoring e2e test to collect differences
    Co-Authored-By: Mark Tyree<72257311+Hard-Luck@users.noreply.github.com>

--- tests/test_future_homes_standard_preprocessing.rs
+++ tests/test_future_homes_standard_preprocessing.rs
@@ -1,8 +1,7 @@
 use home_energy_model::output_writer::FileOutputWriter;
 use home_energy_model::OutputFormat;
 use home_energy_model_wrapper_fhs::{run_wrappers, FhsFlags};
-use itertools::Itertools;
-use serde_json::Value;
+use serde_json::{json, Value};
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
@@ -43,7 +42,9 @@ fn test_demo_file_preprocessing_output() {
     let expected_output = file_value(expected_output_dir, demo_input_file_name);
     let actual_output = file_value(temporary_output_dir, demo_input_file_name);
     delete_temporary_output_directory(temporary_output_dir);
-    preprocessed_input_matches_expected(&actual_output, &expected_output, vec![]);
+    let difference_count =
+        preprocessed_input_matches_expected(&actual_output, &expected_output, vec![]);
+    assert_eq!(difference_count, 0);
 }
 
 fn file_value(directory: &str, file_name: &str) -> Value {
@@ -66,50 +67,202 @@ fn delete_temporary_output_directory(directory: &str) {
     fs::remove_dir_all(temp_output_dir).unwrap();
 }
 
+fn test_number_differences() {
+    let actual = json!(1.0);
+    let expected = json!(1.0);
+    let difference_cout = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_cout, 0);
+
+    let actual = json!(1.0);
+    let expected = json!(1);
+    let difference_cout = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_cout, 0);
+
+    let actual = json!(1.3);
+    let expected = json!(1.0);
+    let difference_cout = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_cout, 1);
+}
+
+fn test_boolean_differences() {
+    let actual = json!(false);
+    let expected = json!(false);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 0);
+
+    let actual = json!(true);
+    let expected = json!(false);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 1);
+}
+
+fn test_string_differences() {
+    let actual = json!("test");
+    let expected = json!("test");
+    let difference_cout = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_cout, 0);
+
+    let actual = json!("t3st");
+    let expected = json!("test");
+    let difference_cout = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_cout, 1);
+}
+
+fn test_array_differences() {
+    let actual = json!([1, 2, 3]);
+    let expected = json!([1, 2, 3]);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 0);
+
+    let actual = json!([1.0, 2, 3]);
+    let expected = json!([1, 2, 3]);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 0);
+
+    let actual = json!([2, 2, 3]);
+    let expected = json!([1, 2, 3]);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 1);
+
+    let actual = json!([3, 3, 3]);
+    let expected = json!([1, 2, 3]);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 2);
+
+    let actual = json!([1, 2, 3]);
+    let expected = json!([1, 2]);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 1);
+}
+
+fn test_array_vs_non_array() {
+    let actual = json!([1, 2, 3]);
+    let expected = json!(1);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 1);
+
+    let actual = json!(1);
+    let expected = json!([1, 2, 3]);
+    let difference_count = preprocessed_input_matches_expected(&actual, &expected, vec![]);
+    assert_eq!(difference_count, 1);
+}
+
+#[test]
+fn test_preprocessed_input_matches_expected() {
+    test_number_differences();
+    test_boolean_differences();
+    test_string_differences();
+    test_array_differences();
+    test_array_vs_non_array();
+}
 pub(crate) fn preprocessed_input_matches_expected(
     actual: &Value,
     expected: &Value,
-    path_to_node: Vec<&str>,
-) {
-    let mut expected_keys = actual.as_object().unwrap().keys().collect_vec();
-    expected_keys.sort();
-    let mut actual_keys = expected.as_object().unwrap().keys().collect_vec();
-    actual_keys.sort();
-    let mut path_to_node = path_to_node;
-
-    assert_eq!(actual_keys, expected_keys);
-    
-    for key in expected_keys {
-        if key == "Events" {
-            continue;
-        }
-
-        path_to_node.push(key);
-
-        let actual_value = &actual[key];
-        let expected_value = &expected[key];
-
-        if expected_value.as_object().is_some() {
-            preprocessed_input_matches_expected(expected_value, actual_value, path_to_node.clone());
-        } else if actual_value != expected_value {
-            if values_match_as_numbers(actual_value, expected_value) {
-                continue;
-            }
-
-            println!(
-                "Expected {:?}, but got {:?} at {:?} \n",
-                expected[key], actual[key], path_to_node
-            )
-        }
-        path_to_node.pop();
+    mut path_to_node: Vec<String>,
+) -> isize {
+    if actual == expected {
+        return 0;
     }
+
+    let mut differences_in_array = |actual: &Value, expected: &Value| -> isize {
+        let actual_as_array = actual.as_array();
+        let expected_as_array = expected.as_array();
+
+        let mut difference_count: isize = 0;
+
+        match (actual_as_array, expected_as_array) {
+            (Some(actual), Some(expected)) => {
+                if actual.len() != expected.len() {
+                    difference_count += 1;
+                    println!(
+                        "Expected array to have length of {:?}, but got {:?} at {:?} \n",
+                        expected.len(),
+                        actual.len(),
+                        path_to_node
+                    );
+                }
+
+                // } else {
+                for (i, expected_item) in expected.iter().enumerate() {
+                    path_to_node.push(i.to_string());
+                    difference_count += preprocessed_input_matches_expected(
+                        &actual[i.clone()],
+                        expected_item,
+                        path_to_node.clone(),
+                    );
+                    path_to_node.pop();
+                }
+                difference_count
+            }
+            (Some(_), None) | (None, Some(_)) => 1,
+            (None, None) => -1,
+        }
+    };
+
+    let array_differences: isize = differences_in_array(actual, expected);
+    if array_differences == -1 {
+        if values_match_as_numbers(actual, expected) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    array_differences
+
+    //
+    // let mut differences_in_object = |actual: &Value, expected: &Value| -> usize {
+    //     let actual_as_object = actual.as_object();
+    //     let expected_as_object = expected.as_object();
+    //     match (actual_as_object, expected_as_object) {
+    //         (Some(actual), Some(expected)) => {
+    //             let mut expected_keys = actual.keys().collect_vec();
+    //             expected_keys.sort();
+    //             let mut actual_keys = expected.keys().collect_vec();
+    //             actual_keys.sort();
+    //             let mut path_to_node = path_to_node;
+    //
+    //
+    //             assert_eq!(actual_keys, expected_keys);
+    //
+    //             for key in expected_keys {
+    //                 if key == "Events" {
+    //                     continue;
+    //                 }
+    //                 let actual_value = &actual[key];
+    //                 let expected_value = &expected[key];
+    //
+    //                 difference_count += preprocessed_input_matches_expected(
+    //                     actual_value,
+    //                     expected_value,
+    //                     path_to_node.clone(),
+    //                     difference_count,
+    //                 );
+    //             }
+    //         }
+    //         _ => todo!(),
+    //     }
+    //
+    //     difference_count
+    // };
+    //
+    //
+    // difference_count += differences_in_object(actual, expected);
+    //
+    // difference_count += 1;
+    //
+    // println!(
+    //     "Expected {:?}, but got {:?} at {:?} \n",
+    //     expected, actual, path_to_node
+    // );
+
+    // difference_count
 }
 
 fn values_match_as_numbers(actual_value: &Value, expected_value: &Value) -> bool {
     let actual_value = actual_value.as_f64();
     let expected_value = expected_value.as_f64();
     let values_are_numbers = actual_value.is_some() && expected_value.is_some();
-    
+
     if values_are_numbers && actual_value.unwrap() == expected_value.unwrap() {
         return true;
     }
