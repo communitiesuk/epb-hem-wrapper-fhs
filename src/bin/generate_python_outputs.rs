a87fa4cd8e1c19e1a53f24bbfe8a61d10995b1ae
Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>
2026-07-09 17:13:02 +0100

    Refactor generate_python_outputs
    Co-Authored-By: Mark Tyree <72257311+Hard-Luck@users.noreply.github.com>
    Co-Authored-By: Rachel Howell <98879818+rachowell@users.noreply.github.com>

--- src/bin/generate_python_outputs.rs
+++ src/bin/generate_python_outputs.rs
@@ -2,43 +2,61 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-const DEMO_FILE_PATH: &str = "examples/input/future_homes_standard/demo_FHS.json";
 const DEMO_FILES_DIR_PATH: &str = "examples/input/future_homes_standard";
 const FHS_PY_ENTRYPOINT_PATH: &str = "./../epb-py-fhs-wrapper/src/bin/fhs.py";
 const FHS_PY_PATH: &str = "./../epb-py-fhs-wrapper";
 const PYTHON_OUTPUT_DIR_PATH: &str = "./../epb-hem-wrapper-fhs/tests/e2e/generated_results";
 
 fn main() {
-    generate_python_outputs().expect("Failed to generate python outputs")
-}
-
-fn generate_python_outputs() -> anyhow::Result<()> {
-    let _ = clear_output_directory();
+    clear_output_directory();
     install_python_fhs_requirements();
-    run_all_python_fhs_files()?;
-
-    Ok(())
+    run_all_python_fhs_files();
 }
 
-fn run_all_python_fhs_files() -> anyhow::Result<()> {
-    for entry in fs::read_dir(DEMO_FILES_DIR_PATH)? {
-        let entry = entry?;
+fn clear_output_directory() {
+    // delete output directory and its contents if it exists
+    let _ = fs::remove_dir_all(PYTHON_OUTPUT_DIR_PATH);
+
+    // create empty output directory
+    fs::create_dir_all(PYTHON_OUTPUT_DIR_PATH).unwrap();
+}
+
+fn install_python_fhs_requirements() {
+    println!("Installing required packages...");
+    run_command(&format!("uv sync --project {}", FHS_PY_PATH));
+}
+
+fn run_all_python_fhs_files() {
+    for entry in fs::read_dir(DEMO_FILES_DIR_PATH).unwrap() {
+        let entry = entry.unwrap();
         let path = entry.path();
         let file_name = entry.file_name();
         if !path.is_dir() && file_name.to_str().unwrap().ends_with(".json") {
-            run_python_fhs(path.to_str().unwrap())?;
+            run_python_fhs_preprocessing(path.to_str().unwrap());
         }
     }
-
-    Ok(())
 }
-fn clear_output_directory() -> anyhow::Result<()> {
-    // delete output directory and its contents if it exists
-    let _ = fs::remove_dir_all(PYTHON_OUTPUT_DIR_PATH); // ignore eror result when directory does not exist
 
-    // create empty output directory
-    fs::create_dir_all(PYTHON_OUTPUT_DIR_PATH)?;
-    Ok(())
+fn run_python_fhs_preprocessing(demo_file_path: &str) {
+    let path = Path::new(demo_file_path);
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+
+    let copied_demo_file_path = [PYTHON_OUTPUT_DIR_PATH, file_name]
+        .iter()
+        .collect::<PathBuf>();
+    let copied_demo_file_path = copied_demo_file_path
+        .to_str()
+        .expect("couldn't make output dir path a string");
+    fs::copy(demo_file_path, copied_demo_file_path).unwrap();
+
+    println!("Running Python FHS..");
+    let run_python_fhs_cmd = format!(
+        "uv run --project {} {} {} --preprocess-only",
+        FHS_PY_PATH, FHS_PY_ENTRYPOINT_PATH, copied_demo_file_path
+    );
+    run_command(&run_python_fhs_cmd);
+    println!("\n");
+    println!("Python FHS ran");
 }
 
 fn run_command(cmd: &str) -> String {
@@ -55,33 +73,4 @@ fn run_command(cmd: &str) -> String {
     println!("{stdout}");
     println!("{stderr}");
     stderr
-}
-
-fn install_python_fhs_requirements() {
-    println!("Installing required packages...");
-    run_command(&format!("uv sync --project {}", FHS_PY_PATH));
-}
-
-fn run_python_fhs(demo_file_path: &str) -> Result<(), anyhow::Error> {
-    let path = Path::new(demo_file_path);
-    let file_name = path.file_name().unwrap().to_str().unwrap();
-
-    let copied_demo_file_path = [PYTHON_OUTPUT_DIR_PATH, file_name]
-        .iter()
-        .collect::<PathBuf>();
-    let copied_demo_file_path = copied_demo_file_path
-        .to_str()
-        .expect("couldn't make output dir path a string");
-    fs::copy(DEMO_FILE_PATH, copied_demo_file_path)?;
-
-    println!("Running Python FHS..");
-    let run_python_fhs_cmd = format!(
-        "uv run --project {} {} {} --preprocess-only",
-        FHS_PY_PATH, FHS_PY_ENTRYPOINT_PATH, copied_demo_file_path
-    );
-    run_command(&run_python_fhs_cmd);
-    println!("\n");
-    println!("Python FHS ran");
-
-    Ok(())
 }
