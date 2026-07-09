e4918eee6ff97fb152644348751745a9615ff4ea
Hard-Luck <tyreemarka@gmail.com>
2026-07-09 15:04:11 +0100

    generate python results for demo_FHS.json
    Co-Authored-By: Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>
    Co-Authored-By: Rachel Howell <98879818+rachowell@users.noreply.github.com>

---
+++ tests/generate_python_outputs.rs
@@ -0,0 +1,74 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+const DEMO_FILE_PATH: &str = "examples/input/future_homes_standard/demo_FHS.json";
+const FHS_PY_ENTRYPOINT_PATH: &str = "./../epb-py-fhs-wrapper/src/bin/fhs.py";
+const FHS_PY_PATH: &str = "./../epb-py-fhs-wrapper";
+const PYTHON_OUTPUT_DIR_PATH: &str = "./../epb-hem-wrapper-fhs/tests/e2e/generated_results";
+
+fn main() {
+    generate_python_outputs().expect("Failed to generate python outputs")
+}
+
+fn generate_python_outputs() -> anyhow::Result<()> {
+    let _ = clear_output_directory();
+    install_python_fhs_requirements();
+    run_python_fhs()?;
+
+    Ok(())
+}
+
+fn clear_output_directory() -> anyhow::Result<()> {
+    // delete output directory and its contents if it exists
+    let _ = fs::remove_dir_all(PYTHON_OUTPUT_DIR_PATH); // ignore eror result when directory does not exist
+
+    // create empty output directory
+    fs::create_dir_all(PYTHON_OUTPUT_DIR_PATH)?;
+    Ok(())
+}
+
+fn run_command(cmd: &str) -> String {
+    println!("{cmd}");
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(cmd)
+        .output()
+        .expect("Failed to run command");
+
+    let stdout = String::from_utf8(output.stdout).expect("Failed to parse stdout as a string");
+    let stderr = String::from_utf8(output.stderr).expect("Failed to parse stderr as a string");
+
+    println!("{stdout}");
+    println!("{stderr}");
+    stderr
+}
+
+fn install_python_fhs_requirements() {
+    println!("Installing required packages...");
+    run_command(&format!("uv sync --project {}", FHS_PY_PATH));
+}
+
+fn run_python_fhs() -> Result<(), anyhow::Error> {
+    let path = Path::new(DEMO_FILE_PATH);
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+
+    let copied_demo_file_path = [PYTHON_OUTPUT_DIR_PATH, file_name]
+        .iter()
+        .collect::<PathBuf>();
+    let copied_demo_file_path = copied_demo_file_path
+        .to_str()
+        .expect("couldn't make output dir path a string");
+    fs::copy(DEMO_FILE_PATH, copied_demo_file_path)?;
+
+    println!("Running Python FHS..");
+    let run_python_fhs_cmd = format!(
+        "uv run --project {} {} {} --preprocess-only",
+        FHS_PY_PATH, FHS_PY_ENTRYPOINT_PATH, copied_demo_file_path
+    );
+    run_command(&run_python_fhs_cmd);
+    println!("\n");
+    println!("Python FHS ran");
+
+    Ok(())
+}
