7ae155a324b4f29f84f6a2ffb2b2553ca1a820e0
rachowell <rachel.howell@madetech.com>
2026-08-04 16:26:18 +0100

    use par_iter in preprocessing test

--- tests/test_future_homes_standard_preprocessing.rs
+++ tests/test_future_homes_standard_preprocessing.rs
@@ -6,12 +6,13 @@ use home_energy_model::OutputFormat;
 use home_energy_model_wrapper_fhs::{run_wrappers, FhsFlags};
 use indexmap::IndexMap;
 use parking_lot::{Mutex, RwLock};
+use rayon::prelude::*;
 use serde_json::{json, Number, Value};
 use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
 use std::io::{BufReader, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::str::from_utf8;
 use std::sync::{Arc, LazyLock};
 
@@ -160,6 +161,8 @@ fn test_fhs_preprocessing_output_against_generated_results() {
         .unwrap()
         .map(|entry| entry.unwrap().path())
         .filter(|path| path.is_file() && path.extension().is_some_and(|ext| ext == "json"))
+        .collect::<Vec<PathBuf>>()
+        .into_par_iter()
         .map(|path| {
             let demo_input_file_name = path.clone();
             let demo_input_file_name = demo_input_file_name.file_stem().unwrap().to_str().unwrap();
