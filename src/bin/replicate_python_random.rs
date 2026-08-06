6d58e58707b083a06c94f31971c2caa9dfdb92e7
rachowell <rachel.howell@madetech.com>
2026-08-06 10:53:18 +0100

    remove unused import
    Co-Authored-By: Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>

--- src/bin/replicate_python_random.rs
+++ src/bin/replicate_python_random.rs
@@ -1,24 +1,19 @@
 #[cfg(test)]
 use anyhow::anyhow;
 #[cfg(test)]
-
 use pyo3::prelude::*;
 
-fn main() {
-
-}
+fn main() {}
 
 #[cfg(test)]
 fn random_number_generation() -> anyhow::Result<Vec<f64>> {
-    let rng = Python::attach(|py| -> PyResult<Py<PyAny>> {
-        make_rng(py, 37)
-    }).map_err( |e| anyhow!(e))?;
+    let rng =
+        Python::attach(|py| -> PyResult<Py<PyAny>> { make_rng(py, 37) }).map_err(|e| anyhow!(e))?;
     let mut random_numbers = Vec::with_capacity(10);
 
     for _ in 0..10 {
-        let random = Python::attach(|py| -> PyResult<f64> {
-            random(py, rng.clone_ref(py))
-        }).map_err( |e| anyhow!(e))?;
+        let random = Python::attach(|py| -> PyResult<f64> { random(py, rng.clone_ref(py)) })
+            .map_err(|e| anyhow!(e))?;
         random_numbers.push(random);
     }
 
@@ -27,23 +22,23 @@ fn random_number_generation() -> anyhow::Result<Vec<f64>> {
 
 #[cfg(test)]
 fn poisson_number_generation() -> anyhow::Result<Vec<Vec<i32>>> {
-   let rng_poisson = Python::attach(|py| -> PyResult<Py<PyAny>> {
-           make_rng_poisson(py, 37)
-       }).map_err( |e| anyhow!(e))?;
+    let rng_poisson = Python::attach(|py| -> PyResult<Py<PyAny>> { make_rng_poisson(py, 37) })
+        .map_err(|e| anyhow!(e))?;
     let mut random_numbers = Vec::with_capacity(10);
 
     for i in 0..10 {
         let random = Python::attach(|py| -> PyResult<Vec<i32>> {
-            rng_poisson.call_method1(py, "poisson", (i as f64/10., 53))?.call_method0(py, "tolist")?.extract(py)
-
-        }).map_err( |e| anyhow!(e))?;
+            rng_poisson
+                .call_method1(py, "poisson", (i as f64 / 10., 53))?
+                .call_method0(py, "tolist")?
+                .extract(py)
+        })
+        .map_err(|e| anyhow!(e))?;
         random_numbers.push(random);
     }
 
     Ok(random_numbers)
-
 }
-
 
 #[cfg(test)]
 #[pyfunction]
@@ -59,7 +54,6 @@ fn random(py: Python<'_>, rng: Py<PyAny>) -> PyResult<f64> {
     rng.call_method0(py, "random")?.extract::<f64>(py)
 }
 
-
 #[cfg(test)]
 #[pyfunction]
 fn make_rng_poisson(py: Python<'_>, seed: u64) -> PyResult<Py<PyAny>> {
@@ -70,12 +64,12 @@ fn make_rng_poisson(py: Python<'_>, seed: u64) -> PyResult<Py<PyAny>> {
 
 #[cfg(test)]
 mod test {
-    use crate::{main, poisson_number_generation, random_number_generation};
+    use crate::{poisson_number_generation, random_number_generation};
 
     #[test]
     fn test_random_number_generation() {
         let actual = random_number_generation().unwrap();
-        let expected = vec!(
+        let expected = vec![
             0.6820045605879779,
             0.09160260807956389,
             0.6178163488614024,
@@ -86,26 +80,54 @@ mod test {
             0.36922983406291854,
             0.5280186220192247,
             0.1078566833027319,
-        );
+        ];
         assert_eq!(actual, expected);
-
     }
     #[test]
     fn test_poisson_number_generation() {
         let actual = poisson_number_generation().unwrap();
-        let expected = vec!(
-            Vec::from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-            Vec::from([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]),
-            Vec::from([0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0]),
-            Vec::from([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0]),
-            Vec::from([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 2, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0]),
-            Vec::from([0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0, 1, 2, 2, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]),
-            Vec::from([2, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 3, 2, 0, 0, 1, 1, 2, 1, 0, 1, 0, 0, 0, 1, 0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 4, 2, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0]),
-            Vec::from([0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 2, 0, 0, 0, 4, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 2, 0, 1, 0, 0, 1, 0, 2, 1, 1, 2, 1, 1, 0, 1, 3, 0, 0, 1, 0, 2, 0, 0, 1, 0, 1, 0, 0]),
-            Vec::from([1, 0, 2, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 1, 2, 2, 0, 0, 1, 2, 0, 1, 0, 2, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 1, 0, 0, 1, 2, 1, 1, 2, 0, 0, 0, 3, 2]),
-            Vec::from([1, 2, 2, 1, 1, 2, 0, 2, 3, 0, 2, 0, 0, 1, 4, 1, 0, 3, 0, 2, 1, 0, 1, 0, 1, 0, 2, 1, 1, 1, 1, 1, 1, 2, 2, 3, 0, 1, 1, 0, 0, 1, 2, 1, 0, 0, 0, 1, 1, 2, 3, 0, 0]),
-        );
+        let expected = vec![
+            Vec::from([
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ]),
+            Vec::from([
+                0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+            ]),
+            Vec::from([
+                0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+            ]),
+            Vec::from([
+                0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 2, 1, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+            ]),
+            Vec::from([
+                0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+                1, 1, 0, 2, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0,
+            ]),
+            Vec::from([
+                0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0, 1, 2, 2, 1, 0, 0,
+                0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0,
+            ]),
+            Vec::from([
+                2, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 3, 2, 0, 0, 1, 1, 2, 1, 0, 1, 0, 0,
+                0, 1, 0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 4, 2, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0,
+            ]),
+            Vec::from([
+                0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 2, 0, 0, 0, 4, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 2, 0, 1,
+                0, 0, 1, 0, 2, 1, 1, 2, 1, 1, 0, 1, 3, 0, 0, 1, 0, 2, 0, 0, 1, 0, 1, 0, 0,
+            ]),
+            Vec::from([
+                1, 0, 2, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 1, 2, 2, 0, 0, 1, 2, 0, 1, 0, 2, 1, 0, 1, 1,
+                2, 1, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 1, 0, 0, 1, 2, 1, 1, 2, 0, 0, 0, 3, 2,
+            ]),
+            Vec::from([
+                1, 2, 2, 1, 1, 2, 0, 2, 3, 0, 2, 0, 0, 1, 4, 1, 0, 3, 0, 2, 1, 0, 1, 0, 1, 0, 2, 1,
+                1, 1, 1, 1, 1, 2, 2, 3, 0, 1, 1, 0, 0, 1, 2, 1, 0, 0, 0, 1, 1, 2, 3, 0, 0,
+            ]),
+        ];
         assert_eq!(expected, actual)
     }
 }
-
